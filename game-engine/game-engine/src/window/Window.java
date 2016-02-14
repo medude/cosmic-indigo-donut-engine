3a7a83d756cf9f8e16c7e65c611fa932a55e2401
medude <nathan@nathan-desktop>
2016-02-14 15:24:57 -0500

    Fixed window class, now inits.

--- game-engine/game-engine/src/window/Window.java
+++ game-engine/game-engine/src/window/Window.java
@@ -6,11 +6,13 @@ public class Window {
 	private static long id;
 	
 	public static long create(){
+		CoreWindow.init();
 		id=CoreWindow.create();
 		return id;
 	}
 	
 	public static long create(String title){
+		CoreWindow.init();
 		id=CoreWindow.create(title);
 		return id;
 	}
