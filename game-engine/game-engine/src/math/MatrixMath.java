c302d969b318b50506c791d21e01c5234a525b92
medude <varner_nathan@yahoo.com>
2016-07-05 16:50:23 -0400

    Worked with things.

---
+++ game-engine/game-engine/src/math/MatrixMath.java
@@ -0,0 +1,11 @@
+package math;
+
+public class MatrixMath {
+	public static Matrix4 dotProduct(Matrix4 matrix1, Matrix4 matrix2){
+		Matrix4 newMatrix = new Matrix4();
+		
+		
+		
+		return newMatrix;
+	}
+}
--- game-engine/game-engine/src/scene/Group.java
+++ game-engine/game-engine/src/scene/Group.java
@@ -1,15 +1,25 @@
 package scene;
 
+import components.types.TransformComponent;
+import math.TransformationMatrix;
 import math.Vector3;
 
 public class Group {
-	public Vector3 position=new Vector3();
+	public TransformComponent localTransform;
+	public TransformComponent worldTransform;
 	public Group[] groupChildren;
 	public Thing[] thingChildren;
 	
 	public Group(Vector3 position, Group[] groupChildren, Thing[] thingChildren){
-		this.position=position;
+		localTransform=new TransformComponent(TransformationMatrix.create(position, new Vector3(0, 0, 0), 1));
+		worldTransform=new TransformComponent(TransformationMatrix.create(position, new Vector3(0, 0, 0), 1));
 		this.groupChildren=groupChildren;
 		this.thingChildren=thingChildren;
+	}
+	
+	public void calculateChildTransform(){
+		for(Group group:groupChildren){
+			group.worldTransform=this.worldTransform;
+		}
 	}
 }
